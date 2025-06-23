e83081c02238098ef98d509655730d3b175bf75d
Mark Moussa <projectprogramamark@gmail.com>
2025-06-22 23:26:23 -0400

    landing page 2025. deploy using gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      '5232-2601-140-8781-1aa0-7cd1-e605-ed8c-547f.ngrok-free.app',
-    ],
-  },
+  server: {},
 })
